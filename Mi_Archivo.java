eb890e61806307366c71f22b7bc3ced66e8d208c
Kgamboa01 <237894439+KennyG885@users.noreply.github.com>
2025-11-12 17:27:24 -0600

    Create Mi_Archivo.java

---
+++ Mi_Archivo.java
@@ -0,0 +1,17 @@
+public class Mi_Archivo{
+
+    public static void main(String[] args){
+        System.out.println("hola, mundo desde java");
+        saludar("nombre");
+        sumar(5, 3);
+    }
+
+    public static void saludar(String nombre) {
+        System.out.println("Hola, " + nombre + ". Bienvenido al ejercicio de git y GitHub!");    
+    }
+
+    public static void sumar(int a, int b) {
+        int resultado = a + b;
+        System.out.println("La suma de " + a + " + " + b + " es: " + resultado);
+    }
+}
